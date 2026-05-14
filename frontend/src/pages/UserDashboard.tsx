4d4abaca0b6925571145439f49f9250b160652d7
Ecmosplasmidou <cassamb97@gmail.>
2026-05-14 02:37:56 +0200

    fix

--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Car, Mail, Plus, Trash2, Save, MapPin, Calendar, Receipt, Download, CreditCard } from 'lucide-react';
+import { Car, Mail, Plus, Trash2, Save, MapPin, Calendar, Receipt, Download } from 'lucide-react';
 import api from '../services/api';
 import { generateInvoicePDF } from '../utils/generateInvoicePDF';
 
